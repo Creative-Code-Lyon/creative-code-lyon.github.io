31db11676ab0a7529ae6342c977bb0a2b59d0f3b
makio135 <radisson@MAC22219-3.local>
2018-09-04 21:14:10 +0200

    add some dynamism via diagonal and bounce

--- js/main.js
+++ js/main.js
@@ -3,121 +3,133 @@ console.clear();
 const s = Splitting();
 ////////////////////////////////////////////////////
 const flatten = (arr, result = []) => {
-  for (let i = 0, length = arr.length; i < length; i++) {
-    const value = arr[i];
-    if (Array.isArray(value)) {
-      flatten(value, result);
-    } else {
-      result.push(value);
+    for (let i = 0, length = arr.length; i < length; i++) {
+        const value = arr[i];
+        if (Array.isArray(value)) {
+            flatten(value, result);
+        } else {
+            result.push(value);
+        }
     }
-  }
-  return result;
+    return result;
 };
 
-const canvas = document.querySelector( 'canvas' );
-const ctx = canvas.getContext( '2d' );
-ctx.strokeStyle= 'white';
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+ctx.strokeStyle = "white";
 let w = window.innerWidth;
 let h = window.innerHeight;
-canvas.width=w;
-canvas.height=h;
+canvas.width = w;
+canvas.height = h;
 
-class Rect{
-  constructor( x, y, w, h ){
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.s = 95;
-  }
-
-  split( n, axis ) {
-    if( n == 0 ) return this;
-
-    let p = ( new Array( n ) ).fill( 0 ).map( d => Math.random() ).sort();
-
-    let newRects = [];
-
-    for( let i = 0; i < n; i ++ ) {
-      let x, y, w, h;
-      if( axis < 0.5 ) {
-        x = this.x + ( i == 0 ? 0 : p[ i - 1 ] * this.w );
-        y = this.y;
-        w = this.w * ( p [ i ] - ( i == 0 ? 0 : p[ i - 1 ] ) );
-        h = this.h;
-      }
-      else {
-        x = this.x;
-        y = this.y + ( i == 0 ? 0 : p[ i - 1 ] * this.h );
-        w = this.w;
-        h = this.h * ( p [ i ] - ( i == 0 ? 0 : p[ i - 1 ] ) );
-      }
-
-      newRects.push( new Rect( x, y, w, h ) );
+class Rect {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.s = 95;
     }
 
-    {
-      let x,y,w,h;
-      if( axis < 0.5 ) {
-        x = this.x + p[ p.length - 1 ] * this.w ;
-        y = this.y;
-        w = this.w * ( 1.0 - p[ p.length - 1 ] );
-        h = this.h;
-      }
-      else {
-        x = this.x;
-        y = this.y + p[ p.length - 1 ] * this.h;
-        w = this.w;
-        h = this.h * ( 1.0 - p [ p.length - 1 ] );
-      }
+    split(n, axis) {
+        if (n == 0) return this;
 
-      newRects.push( new Rect( x, y, w, h ) );
+        let p = new Array(n)
+            .fill(0)
+            .map(d => Math.random())
+            .sort();
+
+        let newRects = [];
+
+        for (let i = 0; i < n; i++) {
+            let x, y, w, h;
+            if (axis < 0.5) {
+                x = this.x + (i == 0 ? 0 : p[i - 1] * this.w);
+                y = this.y;
+                w = this.w * (p[i] - (i == 0 ? 0 : p[i - 1]));
+                h = this.h;
+            } else {
+                x = this.x;
+                y = this.y + (i == 0 ? 0 : p[i - 1] * this.h);
+                w = this.w;
+                h = this.h * (p[i] - (i == 0 ? 0 : p[i - 1]));
+            }
+
+            newRects.push(new Rect(x, y, w, h));
+        }
+
+        let x, y, w, h;
+        if (axis < 0.5) {
+            x = this.x + p[p.length - 1] * this.w;
+            y = this.y;
+            w = this.w * (1.0 - p[p.length - 1]);
+            h = this.h;
+        } else {
+            x = this.x;
+            y = this.y + p[p.length - 1] * this.h;
+            w = this.w;
+            h = this.h * (1.0 - p[p.length - 1]);
+        }
+        newRects.push(new Rect(x, y, w, h));
+        
+        return newRects;
     }
-    return newRects;
-  }
 
-  display(){
-      if( this.axis ){
-          ctx.fillRect( this.x, this.y, this.w * this.s / 100, this.h );
-          // ctx.rect( this.x, this.y, this.w * this.s / 100, this.h );
-      }
-      else{
-          ctx.fillRect( this.x, this.y, this.w, this.h * this.s / 100 );
-          // ctx.rect( this.x, this.y, this.w, this.h * this.s / 100 );
-      }
-      // ctx.stroke();
-  }
+    display() {
+        ctx.fillStyle = red ? "red" : "black";
+        ctx.strokeStyle = red ? "red" : "black";
 
-  anim(){
-      this.axis = Math.random() < 0.5;
-      TweenMax.fromTo( this, .5, { s: 0 }, { s: 100, delay: Math.random() } )
-  }
+        if (this.axis) {
+            ctx.fillRect(this.x, this.y, ((this.w + 1) * this.s) / 100, this.h + 1);
+        } else {
+            ctx.fillRect(this.x, this.y, this.w + 1, ((this.h + 1) * this.s) / 100);
+        }
+    }
+
+    anim() {
+        this.axis = Math.random() < 0.5;
+        TweenMax.fromTo(
+            this,
+            0.2 + Math.random(), {
+                s: 0
+            }, {
+                s: 100,
+                delay: Math.random(),
+                ease: Bounce.easeOut
+            }
+        );
+    }
 }
 
 let rects = [];
 
-function splitRects( n ){
-  rects = rects.map( rect => rect.split( Math.random() * 4 | 0, Math.random() ) );
-  rects = flatten( rects );
+function splitRects(n) {
+    rects = rects.map(rect => rect.split((Math.random() * 4) | 0, Math.random()));
+    rects = flatten(rects);
 
-  if( --n ) splitRects( n );
+    if (--n) splitRects(n);
 }
 
 let red = true;
 const anim = () => {
-  rects = [];
-  rects.push( new Rect( 0, 0, w, h ) );
-  splitRects( 1 + Math.random() * 12 | 0 );
+    rects = [];
+    rects.push(new Rect(-w, -h, 2 * w, 2 * h));
+    splitRects((1 + Math.random() * 12) | 0);
 
-  ctx.fillStyle = red ? 'red' : 'black';
-  red = !red;
-  rects.forEach( rect => rect.anim() );
+    red = !red;
+    rects.forEach(rect => rect.anim());
 };
 anim();
-setInterval( anim, 4000 );
+setInterval(anim, 4000);
 
+(function display() {
+    requestAnimationFrame(display);
 
-( function display(){
-  requestAnimationFrame( display );
-  rects.forEach( rect => rect.display() );
-} )();
+    ctx.fillStyle = red ? "black" : "red";
+    ctx.fillRect(0, 0, w, h);
+    ctx.save();
+    ctx.translate(w / 2, h / 2);
+    ctx.rotate(Math.PI / 7);
+    rects.forEach(rect => rect.display());
+    ctx.restore();
+})();
